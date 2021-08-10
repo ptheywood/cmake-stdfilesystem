7da9d822d6e048812feb8a383d199eba1e985ccc
Peter Heywood <peethwd@gmail.com>
2021-08-10 19:46:12 +0100

    Completely redo macro soup for C++14 gcc support. __has_include is useless

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,59 @@
 #include <stdio.h>
-#if defined(__has_include) && __has_include(<version>)
-#include <version>
-#endif
 #include <string>
 
-// Check for std::filesystem feature flags and header.
-#if defined(__cpp_lib_filesystem) || (defined(__has_include) && __has_include(<filesystem>))
-#include <filesystem>
-namespace fs = std::filesystem;
-#elif defined(__cpp_lib_experimental_filesystem) || (defined(__has_include) && __has_include(<experimental/filesystem>))
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
-#else 
-#error "<filesystem> or <experimental/filesystem> required"
+// Include <version> if it is available for feature macros.
+#if defined(__has_include) 
+    #if __has_include(<version>)
+        #include <version>
+    #endif
 #endif
+
+// Must have complex ifndef soup, as __has_include cannot be used on the same line it is checked for.
+#if defined(__cpp_lib_filesystem) && __cplusplus >= 201703L
+    #define USE_STD_EXPERIMENTAL_FILESYSTEM 0
+#elif defined(__cpp_lib_experimental_filesystem)
+    #define USE_STD_EXPERIMENTAL_FILESYSTEM 1
+#elif defined(__has_include)
+    #if __has_include(<filesystem>) && __cplusplus >= 201703L
+        #define USE_STD_EXPERIMENTAL_FILESYSTEM 0
+    #elif __has_include(<experimental/filesystem>)
+        #define USE_STD_EXPERIMENTAL_FILESYSTEM 1
+    #endif
+#endif
+#if !defined(USE_STD_EXPERIMENTAL_FILESYSTEM)
+    #error "Could not determine support of <filesystem> or <experimental/filesystem>"
+#elif USE_STD_EXPERIMENTAL_FILESYSTEM
+    #include <experimental/filesystem>
+    namespace fs = std::experimental::filesystem;
+#else
+    #include <filesystem>
+    namespace fs = std::filesystem;
+#endif
+// #undef USE_STD_EXPERIMENTAL_FILESYSTEM
 
 int main(int argc, char* argv[]) { 
 
+    #if defined(USE_STD_EXPERIMENTAL_FILESYSTEM)
+        printf("USE_STD_EXPERIMENTAL_FILESYSTEM %d\n", USE_STD_EXPERIMENTAL_FILESYSTEM);
+    #endif
+
     // Printf the output of feature macros.
     #if defined(__cpp_lib_filesystem) 
-        printf("defined(__cpp_lib_filesystem) \n");
+        printf("defined(__cpp_lib_filesystem) %d \n", __cpp_lib_filesystem);
     #endif
-    #if defined(__has_include) && __has_include(<filesystem>)
-        printf("__has_include(<filesystem>)\n");
+    #if defined(__has_include) 
+        #if __has_include(<filesystem>)
+            printf("__has_include(<filesystem>)\n");
+        #endif
     #endif
 
     #if defined(__cpp_lib_experimental_filesystem) 
-        printf("defined(__cpp_lib_filesystem) \n");
+        printf("defined(__cpp_lib_experimental_filesystem) %d \n", __cpp_lib_experimental_filesystem);
     #endif
-    #if defined(__has_include) && __has_include(<experimental/filesystem>)
-        printf("__has_include(<experimental/filesystem>)\n");
+    #if defined(__has_include) 
+        #if __has_include(<experimental/filesystem>)
+            printf("__has_include(<experimental/filesystem>)\n");
+        #endif
     #endif
 
     #if defined(__cplusplus)
