1093699dab96f8a17c98ffe75c0e840e403c2da0
Peter Heywood <peethwd@gmail.com>
2021-08-10 19:04:52 +0100

    Use featuer macros / __has_include on linux for older gcc support in c++17 mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,44 @@
 #include <stdio.h>
+#if defined(__has_include) && __has_include(<version>)
+#include <version>
+#endif
+#include <string>
 
-// @todo - handle different c++ standards
+// 201703L	
 
+// Check for std::filesystem feature flags and header.
+#if defined(__cpp_lib_filesystem) || (defined(__has_include) && __has_include(<filesystem>))
 #include <filesystem>
+namespace fs = std::filesystem;
+#elif defined(__cpp_lib_experimental_filesystem) || (defined(__has_include) && __has_include(<experimental/filesystem>))
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#else 
+#error "<filesystem> or <experimental/filesystem> required"
+#endif
 
 int main(int argc, char* argv[]) { 
     printf("hello world\n");
 
-    std::filesystem::path p = std::filesystem::current_path();
-
+    fs::path p = fs::current_path();
     printf("current_path %s\n", p.c_str());
+
+    #if defined(__cpp_lib_filesystem) 
+        printf("defined(__cpp_lib_filesystem) \n");
+    #endif
+    #if defined(__has_include) && __has_include(<filesystem>)
+        printf("__has_include(<filesystem>)\n");
+    #endif
+
+    #if defined(__cpp_lib_experimental_filesystem) 
+        printf("defined(__cpp_lib_filesystem) \n");
+    #endif
+    #if defined(__has_include) && __has_include(<experimental/filesystem>)
+        printf("__has_include(<experimental/filesystem>)\n");
+    #endif
+
+    #if defined(__cplusplus)
+        printf("__cplusplus %ld\n", __cplusplus);
+    #endif
+
 }
